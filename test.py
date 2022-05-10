7f8253b611459f7c465003c428d348dd8c51f20c
Tibe <35812385+bebcampa@users.noreply.github.com>
2022-05-10 11:45:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from azure.identity import ClientSecretCredential
 from msgraph.core import GraphClient
 import json
 
-clientid= "67f62bc8-c664-4e63-9d44-128673cdc0c2"
-clientsecret = "5Ld8Q~.e5FpnjO50cpOn1vKdFaNXXbS6Gh_Aaa6Y"
-tenantid = "e849ddc8-20b6-406a-aded-d8e20d57e4f0"
+clientid= ""
+clientsecret = ""
+tenantid = ""
 
 
 credentials=ClientSecretCredential(tenant_id=tenantid,client_id=clientid,client_secret=clientsecret) 
